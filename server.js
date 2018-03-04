a614a3baed1908b4303a936be7b4c1962eca7811
szyncel <mtteu1994@gmail.com>
2018-03-04 15:30:35 +0100

    deploy

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 //Install express server
 const express = require('express');
+const path = require('path');
+const http = require('http');
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
 
 /**
  * Get port from environment and store in Express.
  */
 const port = process.env.PORT || '3000';
 app.set('port', port);
+
+
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
